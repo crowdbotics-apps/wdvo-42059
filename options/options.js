f49cfd0ead1204440690d529ddb164d0c2177c10
Crowdbotics <team@crowdbotics.com>
2023-07-10 15:31:48 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "wdvo-42059",
+  url: "https://wdvo-42059.botics.co",
+  api: "https://wdvo-42059.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
